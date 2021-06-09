aeba7600b02f2e17298cb06bb733acbe767b7539
Tequila <ka4xid@gmail.com>
2021-06-09 14:34:43 +0300

    Saving messages with new way
    Because cant walk around single ' in text, db throws error

--- HistoryModule.py
+++ HistoryModule.py
@@ -20,12 +20,8 @@ class HistoryModule:
 
 	def save_message(self, message_id: int, author_id: int, message_date: int, message_text: str):
 		formatted_message_date: str = datetime.fromtimestamp(message_date).strftime("%Y-%m-%d %H:%M:%S")
-
-		query = """INSERT INTO 'messages_history' VALUES('{0}', '{1}', '{2}', '{3}');""".format(message_id, 
-																								author_id, 
-																								formatted_message_date, 
-																								message_text)
-		self.db_handle.exc(query)
+		print(message_text)
+		self.db_handle.exc("""INSERT INTO 'messages_history' VALUES((?), (?), (?), (?));""", (message_id, author_id, formatted_message_date, message_text))
 		self.db_handle.com()
 		#print("{0} {1}\n{2}\n{3}".format(message_id, author_id, formatted_message_date, message_text))
 
