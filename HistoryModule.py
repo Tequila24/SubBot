3ba454d56fb6c897af99fe575d62bb2e27c9011f
Tequila <ka4xid@gmail.com>
2021-07-20 16:40:16 +0300

    History table rename

--- HistoryModule.py
+++ HistoryModule.py
@@ -13,14 +13,14 @@ class HistoryModule:
 	def __init__(self, new_vk_handle: VkLib):
 		self.db_handle = SubDB("sub24_messages")
 		self.vk_handle = new_vk_handle
-		self.db_handle.create_table("messages_history", [	("message_id", "INTEGER"),
+		self.db_handle.create_table("messages", [	("message_id", "INTEGER"),
 															("author_id", "INTEGER"),
 															("date", "TEXT"),
 															("text", "TEXT") ])
 
 	def save_message(self, message_id: int, author_id: int, message_date: int, message_text: str):
 		formatted_message_date: str = datetime.fromtimestamp(message_date).strftime("%Y-%m-%d %H:%M:%S")
-		self.db_handle.exc("""INSERT INTO 'messages_history' VALUES((?), (?), (?), (?));""", (message_id, author_id, formatted_message_date, message_text))
+		self.db_handle.exc("""INSERT INTO 'messages' VALUES((?), (?), (?), (?));""", (message_id, author_id, formatted_message_date, message_text))
 		self.db_handle.com()
 		#print("{0} {1}\n{2}\n{3}".format(message_id, author_id, formatted_message_date, message_text))
 
@@ -29,7 +29,7 @@ class HistoryModule:
 			self.vk_handle.reply(peer_id, "Максимальное количество сообщений из истории - 50")
 			return
 
-		result = self.db_handle.exc("""SELECT * FROM 'messages_history' ORDER BY message_id DESC LIMIT (?);""", (messages_amount, ))
+		result = self.db_handle.exc("""SELECT * FROM 'messages' ORDER BY message_id DESC LIMIT (?);""", (messages_amount, ))
 		if len(result):
 			reply: str = ''
 			result.reverse()
